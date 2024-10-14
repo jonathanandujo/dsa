825c330891d986034c810f89fc3ddc7c1ea4e022
Jonathan Andujo Flores <jonathana@microsoft.com>
2024-10-13 18:22:20 -0600

    No extra space approach

--- CountCycles/Program.cs
+++ CountCycles/Program.cs
@@ -8,6 +8,9 @@ namespace CountCycles
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var arr = new int[] {0,1,2,3};
+            Console.WriteLine(CountCycles(arr));
+            Console.WriteLine(CountCyclesNoExtraSpace(arr));
         }
         public static int CountCycles(int[] arr) {
             int result = 0;
@@ -18,7 +21,16 @@ namespace CountCycles
                 result += 1;
                 VisitCycle(arr,visited, i);
             }
-
+            return result;
+        }
+        public static int CountCyclesNoExtraSpace(int[] arr) {
+            int result = 0;
+            for(int i=0; i < arr.Length; i++){
+                if (arr[i]==-1)
+                    continue;
+                result += 1;
+                VisitCycleNoExtraSpace(arr, i);
+            }
             return result;
         }
         private static void VisitCycle(int[] arr, bool[] visited, int index) {
@@ -26,6 +38,14 @@ namespace CountCycles
                 return;
             visited[index] = true;
             VisitCycle(arr, visited, arr[index]);
+        }
+        private static void VisitCycleNoExtraSpace(int[] arr, int index)
+        {
+            if (arr[index] == -1)
+                return;
+            int nextIndex = arr[index];
+            arr[index]=-1;
+            VisitCycleNoExtraSpace(arr,nextIndex);
         }
     }
 
@@ -35,11 +55,15 @@ namespace CountCycles
         public void ShouldPass3(int[] arr) {
             var result = Program.CountCycles(arr);
             Assert.AreEqual(result, 3);
+            result = Program.CountCyclesNoExtraSpace(arr);
+            Assert.AreEqual(result, 3);
         }
 
         [TestCase(new int[]{0,1,2,3,4})]
         public void ShouldPass5(int[] arr) {
             var result = Program.CountCycles(arr);
+            Assert.AreEqual(result, 5);
+            result = Program.CountCyclesNoExtraSpace(arr);
             Assert.AreEqual(result, 5);
         }
     }
