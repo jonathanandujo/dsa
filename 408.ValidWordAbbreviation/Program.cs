7517e36dd2885973fe366e4b82e215a86af99318
Jonathan Andujo Flores <jonathana@microsoft.com>
2024-10-21 21:16:56 -0600

    Adding 408 from leetcode (easy)

---
+++ 408.ValidWordAbbreviation/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(ValidWordAbbreviation("apple","a3e")); //True
+Console.WriteLine(ValidWordAbbreviation("apple","a2e")); //False
+Console.WriteLine(ValidWordAbbreviation("apple","a0e")); //False
+Console.WriteLine(ValidWordAbbreviation("apple","a30")); //False
+Console.WriteLine(ValidWordAbbreviation("internationalization","i5a11o1")); //True
+
+
+bool ValidWordAbbreviation(string word, string abbr) {
+	int wordIndex=0, number=0;
+	for(int i=0; i<abbr.Length;i++)
+	{
+		if(char.IsDigit(abbr[i]))
+		{
+			number = number * 10 + abbr[i] - '0'; //-'0' means will convert the char in ascii to integer
+			if(number == 0)
+				return false;
+		}
+		else
+		{
+			wordIndex += number+1; //1
+			number=0;
+
+			if (word.Length < wordIndex || word[wordIndex-1] != abbr[i])
+				return false;
+		}
+	}
+	return wordIndex + number == word.Length;
+}
--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -7,9 +7,9 @@ namespace Factorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(Factorial(4));
+            Console.WriteLine(Factorial(20));
         }
-        static int Factorial(int n){
+        static long Factorial(long n){
             if (n == 1)
                 return n;
             return n*(Factorial(n-1));
