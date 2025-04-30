6255d3750df9b69d9fe3cc8fb5d75becce1e398d
Jonathan Andujo Flores <jonathana@microsoft.com>
2025-04-30 10:45:33 -0600

    Removing Nu challenge

--- NuCli/CapitalGains/Application/ITaxCalculatorService.cs
+++
@@ -1,8 +0,0 @@
-using Domain.Models;
-
-namespace Application;
-
-public interface ITaxCalculator
-{
-    List<TaxResult> CalculateTaxes(List<StockOperation> operations);
-}
--- NuCli/CapitalGains/Infrastructure/Utils/GlobalConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Infrastructure.Utils;
-
-public static class GlobalConfig
-{
-    public const decimal TaxRate = 0.2m;
-    public const int MinTaxableAmount = 20000;
-    public static bool Debug = false;
-}
